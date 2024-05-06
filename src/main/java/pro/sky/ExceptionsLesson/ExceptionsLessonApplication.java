a98b7af4ad8d6563ac81c4704c53a0e76baad219
REIGaruki <gyakujo@mail.ru>
2024-05-06 15:12:48 +0300

    Initial C

---
+++ src/main/java/pro/sky/ExceptionsLesson/ExceptionsLessonApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.ExceptionsLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsLessonApplication.class, args);
+	}
+
+}
