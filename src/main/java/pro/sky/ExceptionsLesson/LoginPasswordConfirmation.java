7229b055faf7af696191380ea8e784bd603fe15a
REIGaruki <gyakujo@mail.ru>
2024-05-06 15:19:32 +0300

    New Branch

---
+++ src/main/java/pro/sky/ExceptionsLesson/LoginPasswordConfirmation.java
@@ -0,0 +1,4 @@
+package pro.sky.ExceptionsLesson;
+
+public interface LoginPasswordConfirmation {
+}
---
+++ src/main/java/pro/sky/ExceptionsLesson/LoginPasswordConfirmationImpl.java
@@ -0,0 +1,4 @@
+package pro.sky.ExceptionsLesson;
+
+public class LoginPasswordConfirmationImpl implements LoginPasswordConfirmation{
+}
